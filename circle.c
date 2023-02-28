7b9665acbc297521baf4a69948e81466e6726bb6
sapate vaibhav <64630274+sapatevaibhav@users.noreply.github.com>
2023-02-28 19:52:30 +0530

    start of repo

---
+++ circle.c
@@ -0,0 +1,40 @@
+#include <graphics.h>
+#include <stdio.h>
+
+void drawCircle(int xc, int yc, int r)
+{
+    int x = 0, y = r;
+    int d = 3 - 2 * r;
+    while (y >= x)
+    {
+        putpixel(xc + x, yc - y, WHITE);
+        putpixel(xc - x, yc - y, WHITE);
+        putpixel(xc + x, yc + y, WHITE);
+        putpixel(xc - x, yc + y, WHITE);
+        putpixel(xc + y, yc - x, WHITE);
+        putpixel(xc - y, yc - x, WHITE);
+        putpixel(xc + y, yc + x, WHITE);
+        putpixel(xc - y, yc + x, WHITE);
+        x++;
+        if (d > 0)
+        {
+            y--;
+            d = d + 4 * (x - y) + 10;
+        }
+        else
+        {
+            d = d + 4 * x + 6;
+        }
+    }
+}
+
+int main()
+{
+    int gd = DETECT, gm;
+    int xc = 150, yc = 150, r = 50;
+    initgraph(&gd, &gm, "");
+    drawCircle(xc, yc, r);
+    getch();
+    closegraph();
+    return 0;
+}
